d4567244a6d179bb3929746e8dcca0a238f7fd66
FilipeLealves <lealves.work@gmail.com>
2022-06-03 20:42:39 -0300

    new

--- src/domain/ContaCorrente.java
+++ src/domain/ContaCorrente.java
@@ -1,25 +1,60 @@
 package domain;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ContaCorrente {
 	public int id;
 	public double limite;
 	public double saldo;
 	public double valorLimite;
 	
-	public void sacar() {
+	ArrayList<Double> saques = new ArrayList<>();
+	ArrayList<Double> depositos = new ArrayList<>();
+	
+	Random gerador = new Random();
+	
+	public void sacar(double valor) {
+		try {
+			if(valor < 0) throw new Exception ("Valor negativo!");
+			
+			this.saldo -= valor;
+			
+			
+			this.id = gerador.nextInt(9999, 100000 - 1);
+
+			saques.add(valor);
+			
+			System.out.println("Valor do saque: " + valor);
+		} catch(Exception e){
+			System.out.println(e);
+		}
 		
 	}
 	
-	public void depositar() {
-		
+	public void depositar(double valor) {
+		try {
+			this.saldo += valor;
+			
+			this.id = gerador.nextInt(9999, 100000 - 1);
+			depositos.add(this.id, valor);
+			
+		} catch(Exception e){
+			System.out.println(e);{
+			
+			}
+		}
 	}
 	
-	public void setValorLimite() {
-		
+	public void setValorLimite(double valor) {
+		this.valorLimite = valor;
 	}
 	
-	public void cancelarSaque() {
-		
+	public void cancelarSaque(int valor) {
+		for(Double saque : saques) {
+			System.out.println(saque);
+		}
 	}
 	
 	public void cancelarDeposito() {
--- src/domain/Programm.java
+++ src/domain/Programm.java
@@ -3,7 +3,13 @@ package domain;
 public class Programm {
 
 	public static void main(String[] args) {
+		ContaCorrente conta = new ContaCorrente();
 		
+		conta.sacar(100);
+		//conta.sacar(500);
+		//conta.sacar(100);
+		
+		//conta.cancelarSaque();
 	}
 
 }
