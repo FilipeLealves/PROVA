80efee9c330bba4b794a7f9c95f4a39b57ec483c
FilipeLealves <lealves.work@gmail.com>
2022-06-03 19:42:40 -0300

    new

---
+++ src/domain/ContaCorrente.java
@@ -0,0 +1,33 @@
+package domain;
+
+public class ContaCorrente {
+	public int id;
+	public double limite;
+	public double saldo;
+	public double valorLimite;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void setValorLimite() {
+		
+	}
+	
+	public void cancelarSaque() {
+		
+	}
+	
+	public void cancelarDeposito() {
+		
+	}
+	
+	public int qtdTransacoes() {
+		return 0;
+	}
+	
+}
