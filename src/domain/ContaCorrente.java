50b7d714965ba23ec6848c964784e2c838e4a4f6
FilipeLealves <lealves.work@gmail.com>
2022-06-03 21:05:22 -0300

    aaa

--- src/domain/ContaCorrente.java
+++ src/domain/ContaCorrente.java
@@ -3,6 +3,8 @@ package domain;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ContaCorrente {
 	public int id;
@@ -10,23 +12,23 @@ public class ContaCorrente {
 	public double saldo;
 	public double valorLimite;
 	
-	ArrayList<Double> saques = new ArrayList<>();
-	ArrayList<Double> depositos = new ArrayList<>();
+	ArrayList<String> saques = new ArrayList<>();
+	ArrayList<String> depositos = new ArrayList<>();
+	
 	
 	Random gerador = new Random();
 	
 	public void sacar(double valor) {
 		try {
 			if(valor < 0) throw new Exception ("Valor negativo!");
+			if(valor > limite) throw new Exception("Limite indisponível!");
 			
 			this.saldo -= valor;
-			
-			
 			this.id = gerador.nextInt(9999, 100000 - 1);
-
-			saques.add(valor);
 			
-			System.out.println("Valor do saque: " + valor);
+			
+			
+			System.out.println("ID: " + "Valor do saque: " + valor);
 		} catch(Exception e){
 			System.out.println(e);
 		}
@@ -35,10 +37,10 @@ public class ContaCorrente {
 	
 	public void depositar(double valor) {
 		try {
+			if(valor < 0) throw new Exception ("Valor negativo!");
 			this.saldo += valor;
 			
 			this.id = gerador.nextInt(9999, 100000 - 1);
-			depositos.add(this.id, valor);
 			
 		} catch(Exception e){
 			System.out.println(e);{
@@ -48,11 +50,16 @@ public class ContaCorrente {
 	}
 	
 	public void setValorLimite(double valor) {
-		this.valorLimite = valor;
+		try {
+			if(valor < 0) throw new Exception ("Valor negativo!");
+			this.valorLimite = valor;
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 	
 	public void cancelarSaque(int valor) {
-		for(Double saque : saques) {
+		for(String saque : saques) {
 			System.out.println(saque);
 		}
 	}
